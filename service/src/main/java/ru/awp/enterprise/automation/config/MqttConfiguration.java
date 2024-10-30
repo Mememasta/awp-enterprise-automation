63d646f94b15a5fc788a968402b858ba9e67b5f3
pupa <MAChuchalov@stud.kpfu.ru>
2024-10-30 20:51:51 +0300

    added mqtt

--- service/src/main/java/ru/awp/enterprise/automation/config/MqttConfiguration.java
+++ service/src/main/java/ru/awp/enterprise/automation/config/MqttConfiguration.java
@@ -1,68 +1,69 @@
 package ru.awp.enterprise.automation.config;
 
-import org.eclipse.paho.mqttv5.client.IMqttAsyncClient;
-import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
-import org.eclipse.paho.mqttv5.client.persist.MqttDefaultFilePersistence;
+import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
 import org.springframework.integration.mqtt.core.ClientManager;
-import org.springframework.integration.mqtt.core.Mqttv5ClientManager;
-import org.springframework.integration.mqtt.inbound.Mqttv5PahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.outbound.Mqttv5PahoMessageHandler;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.Mqttv3ClientManager;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 @Configuration
 public class MqttConfiguration {
 
-//    @Bean
-//    public ClientManager<IMqttAsyncClient, MqttConnectionOptions> clientManager() {
-//        MqttConnectionOptions connectionOptions = new MqttConnectionOptions();
-//        connectionOptions.setServerURIs(new String[]{"tcp://134.0.113.182:1883"});
-//        connectionOptions.setConnectionTimeout(30000);
-//        connectionOptions.setMaxReconnectDelay(1000);
-//        connectionOptions.setAutomaticReconnect(true);
-//        connectionOptions.setUserName("passIoT");
-//        connectionOptions.setPassword("pass18IoT23".getBytes());
-//        Mqttv5ClientManager clientManager = new Mqttv5ClientManager(connectionOptions, "marat/pupa");
-//        clientManager.setPersistence(new MqttDefaultFilePersistence());
-//        return clientManager;
-//    }
-//
-//    @Bean
-//    public IntegrationFlow mqttInFlow(ClientManager<IMqttAsyncClient, MqttConnectionOptions> clientManager) {
-//        Mqttv5PahoMessageDrivenChannelAdapter messageProducer =
-//                new Mqttv5PahoMessageDrivenChannelAdapter(clientManager, "marat/pupa");
-//        messageProducer.setPayloadType(String.class);
-//        messageProducer.setManualAcks(true);
-//
-//        return IntegrationFlow.from(messageProducer)
-//                .channel(c -> c.queue("fromMqttChannel"))
-//                .get();
-//    }
-//
-//    @Bean
-//    public IntegrationFlow mqttOutFlow(ClientManager<IMqttAsyncClient, MqttConnectionOptions> clientManager) {
-//        var messageHandler = new Mqttv5PahoMessageHandler(clientManager);
-//        messageHandler.setAsync(true);
-//        messageHandler.setAsyncEvents(true);
-//        return f -> f.handle(messageHandler);
-//    }
-
     @Bean
-    public IntegrationFlow mqttInFlow() {
-        MqttConnectionOptions connectionOptions = new MqttConnectionOptions();
-        connectionOptions.setServerURIs(new String[]{"wss://134.0.113.182:8883"});
+    public ClientManager<IMqttAsyncClient, MqttConnectOptions> clientManager() {
+        MqttConnectOptions connectionOptions = new MqttConnectOptions();
+        connectionOptions.setServerURIs(new String[]{"wss://134.0.113.182:1883"});
         connectionOptions.setConnectionTimeout(30000);
         connectionOptions.setMaxReconnectDelay(1000);
         connectionOptions.setAutomaticReconnect(true);
         connectionOptions.setUserName("passIoT");
-        connectionOptions.setPassword("pass18IoT".getBytes());
-        return IntegrationFlow.from(
-                        new Mqttv5PahoMessageDrivenChannelAdapter(
-                                connectionOptions, "subscriber-for-shared-subscription", "marat/pupa"))
-                .<byte[], String>transform(String::new)
-                .handle(m -> System.out.println("Received message: " + m.getPayload()), e -> e.id("dataHandler"))
-                .get();
-
+        connectionOptions.setPassword("pass18IoT".toCharArray());
+        return new Mqttv3ClientManager(connectionOptions, "marat/pupa");
     }
+
+    @Bean
+    public MessageProducer inbound() {
+        MqttPahoMessageDrivenChannelAdapter adapter =
+                new MqttPahoMessageDrivenChannelAdapter(clientManager(),
+                        "marat/pupa");
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(1);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                System.out.println(message.getPayload());
+            }
+
+        };
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+
 }
