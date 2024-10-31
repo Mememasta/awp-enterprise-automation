076baadb2217f99db2ba9b2529048e6a5a261e85
pupa <pu@pa.com>
2024-10-31 16:34:47 +0000

    update listener

--- service/src/main/java/ru/awp/enterprise/automation/config/MqttConfiguration.java
+++ service/src/main/java/ru/awp/enterprise/automation/config/MqttConfiguration.java
@@ -1,20 +1,14 @@
 package ru.awp.enterprise.automation.config;
 
-import org.eclipse.paho.client.mqttv3.IMqttAsyncClient;
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
-import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.core.MessageProducer;
-import org.springframework.integration.mqtt.core.ClientManager;
 import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
 import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
-import org.springframework.integration.mqtt.core.Mqttv3ClientManager;
 import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
-import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
 import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -23,27 +17,31 @@ import org.springframework.messaging.MessagingException;
 
 @Configuration
 public class MqttConfiguration {
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
 
     @Bean
-    public ClientManager<IMqttAsyncClient, MqttConnectOptions> clientManager() {
-        MqttConnectOptions connectionOptions = new MqttConnectOptions();
-        connectionOptions.setServerURIs(new String[]{"wss://134.0.113.182:1883"});
-        connectionOptions.setConnectionTimeout(30000);
-        connectionOptions.setMaxReconnectDelay(1000);
-        connectionOptions.setAutomaticReconnect(true);
-        connectionOptions.setUserName("passIoT");
-        connectionOptions.setPassword("pass18IoT".toCharArray());
-        return new Mqttv3ClientManager(connectionOptions, "marat/pupa");
+    public MqttPahoClientFactory clientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+        options.setServerURIs(new String[]{ "ws://localhost:8883" });
+	options.setCleanSession(true);
+	options.setAutomaticReconnect(false);
+	options.setUserName("passIoT");
+	options.setPassword("pass18IoT".toCharArray());
+        factory.setConnectionOptions(options);
+        return factory;
     }
 
     @Bean
     public MessageProducer inbound() {
-        MqttPahoMessageDrivenChannelAdapter adapter =
-                new MqttPahoMessageDrivenChannelAdapter(clientManager(),
-                        "marat/pupa");
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter("siSampleConsumer", clientFactory(), "marat/pupa");
+
         adapter.setCompletionTimeout(5000);
         adapter.setConverter(new DefaultPahoMessageConverter());
-        adapter.setQos(1);
+        adapter.setQos(0);
         adapter.setOutputChannel(mqttInputChannel());
         return adapter;
     }
@@ -52,18 +50,11 @@ public class MqttConfiguration {
     @ServiceActivator(inputChannel = "mqttInputChannel")
     public MessageHandler handler() {
         return new MessageHandler() {
-
             @Override
             public void handleMessage(Message<?> message) throws MessagingException {
-                System.out.println(message.getPayload());
+                System.out.println("MESSAGE: " + message.getPayload());
             }
 
         };
     }
-
-    @Bean
-    public MessageChannel mqttInputChannel() {
-        return new DirectChannel();
-    }
-
 }
